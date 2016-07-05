16dda3c2b6f65ab962dc866275974c8646375151
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-07-05 19:33:25 +0300

    add animation for button menu

--- contact-form/js/main.js
+++ contact-form/js/main.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
         }
 
     });
+
+    $(".mini-nav").on("click", function(){
+        $(this).find(".item2").fadeOut("slow");
+        $(this).find(".item1").css({
+            "top": "22px",
+            "transform": "rotate(45deg)"});
+        $(this).find(".item3").css({
+            "bottom": "22px",
+            "transform": "rotate(-45deg)"});
+    });
+
+
 });
