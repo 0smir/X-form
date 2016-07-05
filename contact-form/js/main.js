3868424fd842dc011a9657db2a6b5185ac037e39
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-07-05 13:57:51 +0300

    add styles for mobile nav

---
+++ contact-form/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("header nav").find(".btn-search").on("click", function(){
+        if($("#main-search").hasClass("visible")){
+            $("#main-search").removeClass("visible");
+        }
+        else{
+            $("#main-search").addClass("visible");
+        }
+
+    });
+});
