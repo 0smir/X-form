9c2e2af71e559a1c17189421e1ef4d04e9c097b9
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-07-05 21:51:36 +0300

    task#: add mobile menu

--- contact-form/js/main.js
+++ contact-form/js/main.js
@@ -9,15 +9,43 @@ $(document).ready(function(){
 
     });
 
-    $(".mini-nav").on("click", function(){
-        $(this).find(".item2").fadeOut("slow");
-        $(this).find(".item1").css({
-            "top": "22px",
-            "transform": "rotate(45deg)"});
-        $(this).find(".item3").css({
-            "bottom": "22px",
-            "transform": "rotate(-45deg)"});
-    });
 
+    // $(".mini-nav").on("click", function(){
+    //     $(this).find(".item2").hide();
+    //     $(this).find(".item1").css({
+    //         "top": "22px",
+    //         "transform": "rotate(45deg)"});
+    //     $(this).find(".item3").css({
+    //         "bottom": "22px",
+    //         "transform": "rotate(-45deg)"});
+    // });
+
+    
+            $("#menu").mmenu({
+                "slidingSubmenus": false,
+                "extensions": [
+                    "pagedim-black"
+                ],
+                "offCanvas": {
+                    "position": "right"
+                },
+                "navbars": [
+                    {
+                        "position": "top",
+                        "content": [
+                            "searchfield"
+                        ]
+                    },
+                    {
+                        "position": "bottom",
+                        "content": [
+                            "<a class='fa fa-envelope' href='#/'></a>",
+                            "<a class='fa fa-twitter' href='#/'></a>",
+                            "<a class='fa fa-facebook' href='#/'></a>"
+                        ]
+                    }
+                ]
+            });
+     
 
 });
---
+++ contact-form/js/mobil-menu.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+        $("#menu").mmenu({
+            "slidingSubmenus": false,
+            "extensions": [
+                "pagedim-black"
+            ],
+            "offCanvas": {
+                "position": "right"
+            },
+            "navbars": [
+                {
+                    "position": "top",
+                    "content": [
+                        "searchfield"
+                    ]
+                },
+                {
+                    "position": "bottom",
+                    "content": [
+                        "<a class='fa fa-envelope' href='#/'></a>",
+                        "<a class='fa fa-twitter' href='#/'></a>",
+                        "<a class='fa fa-facebook' href='#/'></a>"
+                    ]
+                }
+            ]
+        });
+
+    var API = $("#menu").data("mmenu");
+
+    $(".mini-nav").click(function() {
+        API.open();
+    });
+});
