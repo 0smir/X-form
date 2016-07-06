f597497c5c9b82a198bb25a96fbcde25f5cb88df
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-07-06 11:27:49 +0300

    add shadows for buttons and inputs

--- contact-form/js/mobil-menu.js
+++ contact-form/js/mobil-menu.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
 
-        $("#menu").mmenu({
-            "slidingSubmenus": false,
-            "extensions": [
-                "pagedim-black"
-            ],
-            "offCanvas": {
-                "position": "right"
+
+    $("#menu").mmenu({
+        "slidingSubmenus": false,
+        "extensions": [
+            "pagedim-black"
+        ],
+        "offCanvas": {
+            "position": "right"
+        },
+        "navbars": [
+            {
+                "position": "top",
+                "content": [
+                    "searchfield"
+                ]
             },
-            "navbars": [
-                {
-                    "position": "top",
-                    "content": [
-                        "searchfield"
-                    ]
-                },
-                {
-                    "position": "bottom",
-                    "content": [
-                        "<a class='fa fa-envelope' href='#/'></a>",
-                        "<a class='fa fa-twitter' href='#/'></a>",
-                        "<a class='fa fa-facebook' href='#/'></a>"
-                    ]
-                }
-            ]
-        });
+            {
+                "position": "bottom",
+                "content": [
+                    "<a class='fa fa-envelope' href='#/'></a>",
+                    "<a class='fa fa-twitter' href='#/'></a>",
+                    "<a class='fa fa-facebook' href='#/'></a>"
+                ]
+            }
+        ]
+    });
+
 
     var API = $("#menu").data("mmenu");
 
     $(".mini-nav").click(function() {
+
         API.open();
+
     });
+    
+
+
+
 });
